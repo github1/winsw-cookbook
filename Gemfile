48027dc111298b85a7986743ae71e62a023be471
github1 <john.collier4@gmail.com>
2018-11-26 23:08:30 -0500

    configure guard

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'chefspec'
+gem 'chefspec', '7.3.2'
+gem 'foodcritic', '14.3.0'
+gem 'guard', '2.15.0'
+gem 'guard-rspec', '4.7.3'
+gem 'guard-foodcritic', '3.0.0'
 
 group :integration do
   gem 'test-kitchen', '1.23.2'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,15 @@
 node['winsw']['service'].each do |service_name, service_info|
   winsw service_name do
+    service_name service_info['service_name']
+    service_description service_info['service_description']
+    windows_service_name service_info['windows_service_name']
     action service_info['action']
     basedir service_info['basedir']
     enabled service_info['enabled']
     executable service_info['executable']
     args service_info['args']
     env_variables service_info['env_variables']
-    options service_info['options'] if service_info['options']
+    options service_info['options']
     supported_runtimes service_info['supported_runtimes']
   end
 end
